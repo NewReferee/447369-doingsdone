a4b99355e0dbeea2e107f392cd464a8012b37410
NewReferee <drakone2010@gmail.com>
2019-02-13 22:27:02 +0300

    База данных интегрирована

--- functions.php
+++ functions.php
@@ -1,7 +1,6 @@
 <?php 
 
 // Шаблонизатор
-
 function include_template($name, $data) {
 	$name = 'templates/' . $name;
 	$result = '';
@@ -19,8 +18,31 @@ function include_template($name, $data) {
 	return $result;
 }
 
-// Защита от XSS
+// Подключение к базе данных и возврат ресурса подключения
+function database_init ($hostname, $username, $password, $servername) {
+	$connect = mysqli_connect($hostname, $username, $password, $servername);
+	if (!$connect) {
+		print ('Ошибка подключения: ' . mysqli_connect_error());
+	}
+	else {
+	mysqli_set_charset($connect, "utf8");
+	return $connect;
+	}
+}
 
+// Чтение базы данных и возврат ассоциативного массива, где ключ - номер записи, а значение - ассоциативный массив (поле => значение)
+function database_read ($connect, $database_command) {
+	$database_result = mysqli_query($connect, $database_command);
+	if ($database_result) {
+		$database_assoc = mysqli_fetch_all($database_result, MYSQLI_ASSOC);
+		return $database_assoc;
+	}
+	else {
+		print ('Ошибка запроса: ' . mysqli_error($database_result));
+	}
+}
+
+// Защита от XSS
 function xss_protect (&$tasks) {
 	foreach ($tasks as $task_key => $task_values) {
 		foreach ($task_values as $key => $value) {
@@ -30,11 +52,10 @@ function xss_protect (&$tasks) {
 }
 
 // Количества задач для данной категории
-
 function get_tasks ($tasks, $category) {
 	$counter = 0;
   foreach ($tasks as $task_values) {
-      if ($task_values['category'] === $category) {
+      if ($task_values['category_name'] === $category) {
           $counter++;
       }
   }
@@ -42,12 +63,11 @@ function get_tasks ($tasks, $category) {
 }
 
 // Срочность задач
-
 function get_soon ($tasks) {
 	date_default_timezone_set('Europe/Moscow');
 	foreach ($tasks as $task_values) {
-		$time_left = (strtotime($task_values['date']) - time()) / 3600;
-		if ($task_values['date']	!==	'Нет' && $time_left <= 24) {
+		$time_left = (strtotime($task_values['date_require']) - time()) / 3600;
+		if ($task_values['date_require'] != null && $time_left <= 24) {
 			$soon[] = true;
 		}
 		else {
--- index.php
+++ index.php
@@ -2,50 +2,26 @@
 require_once ('functions.php');
 $page_title = 'Дела в порядке';
 $show_complete_tasks = rand(0, 1);
-$category_list = ['Входящие', 'Учеба', 'Работа', 'Домашние дела', 'Авто'];
-$tasks = [
-	'task_1' => [
-			'desc' => 'Собеседование в IT компании',
-			'date' => '09.02.2019',
-			'category' => 'Работа',
-			'state' => false
-	],
 
-	'task_2' => [
-			'desc' => 'Выполнить тестовое задание',
-			'date' => '25.12.2019',
-			'category' => 'Работа',
-			'state' => false
-	],
+// Работа с базой данных
+$connect = database_init("localhost", "root", "", "doingsdone");
+$database_command = 
+	'SELECT category_name
+	FROM category_list
+	WHERE user_id = 3;';
 
-	'task_3' => [
-			'desc' => 'Сделать задание первого раздела',
-			'date' => '09.02.2019',
-			'category' => 'Учёба',
-			'state' => true
-	],
+$category_list = database_read($connect, $database_command);
+$category_list = array_column($category_list, 'category_name');
 
-	'task_4' => [
-			'desc' => 'Встреча с другом',
-			'date' => '22.12.2019',
-			'category' => 'Входящие',
-			'state' => false
-	],
+$database_command =
+	'SELECT task_desc, date_require, 
+	(SELECT category_list.category_name
+	FROM category_list
+	WHERE tasks.category_id = category_list.category_id) AS category_name, task_state
+	FROM tasks
+	WHERE tasks.user_id = 3;';
 
-	'task_5' => [
-			'desc' => 'Купить корм для кота',
-			'date' => 'Нет',
-			'category' => 'Домашние дела',
-			'state' => false
-	],
-
-	'task_6' => [
-			'desc' => 'Заказать пиццу',
-			'date' => 'Нет',
-			'category' => 'Домашние дела',
-			'state' => false
-	],
-];
+$tasks = database_read($connect, $database_command);
 
 // Дополнительная защита от XSS
 xss_protect ($tasks);
@@ -54,7 +30,6 @@ xss_protect ($tasks);
 $soon = get_soon($tasks);
 
 // Шаблонизация
-
 $page_content = include_template ('index.php', [
 	'tasks' => $tasks, 
 	'show_complete_tasks' => $show_complete_tasks,
