476daca8660a8f75eb9109bd275b1e67f8395982
NewReferee <drakone2010@gmail.com>
2019-02-13 22:56:47 +0300

    Добавлен config.php и переменная current_user

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+$page_title = 'Дела в порядке';
+$show_complete_tasks = rand(0, 1);
+$current_user = 3;
+?>
--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php
 require_once ('functions.php');
-$page_title = 'Дела в порядке';
-$show_complete_tasks = rand(0, 1);
+require_once ('config.php');
 
 // Работа с базой данных
 $connect = database_init("localhost", "root", "", "doingsdone");
 $database_command = 
 	'SELECT category_name
 	FROM category_list
-	WHERE user_id = 3;';
+	WHERE user_id = ' . $current_user .';';
 
 $category_list = database_read($connect, $database_command);
 $category_list = array_column($category_list, 'category_name');
@@ -19,7 +18,7 @@ $database_command =
 	FROM category_list
 	WHERE tasks.category_id = category_list.category_id) AS category_name, task_state
 	FROM tasks
-	WHERE tasks.user_id = 3;';
+	WHERE tasks.user_id = ' . $current_user . ';';
 
 $tasks = database_read($connect, $database_command);
 
