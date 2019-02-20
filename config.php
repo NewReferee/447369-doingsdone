62a511e38617a07f46c07b31473738c7ec2e1b8e
NewReferee <drakone2010@gmail.com>
2019-02-20 06:40:59 +0300

    Мелкая правка конфига

--- config.php
+++ config.php
@@ -9,8 +9,5 @@ $show_complete_tasks = 1;
 $current_user = 3;
 
 // URI недоступные для чтения
-$lock = ['/config.php', '/Contributing.md', '/flatpickr.js', '/functions.php','/index.php', '/mysql_helper.php', '/queries.sql',
-'/Readme.md', '/schema.sql', '/script.js', '/css/flatpickr.min.css', '/css/normalize.css', '/css/style.css', '/fonts/opensans-bold-webfont.woff',
-'/fonts/opensans-bold-webfont.woff2', '/fonts/opensans-regular-webfont.woff', '/fonts/opensans-regular-webfont.woff2',
-'/templates/index.php', '/templates/layout.php'];
+$lock = ['/index.php'];
 ?>
