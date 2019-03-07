33f7d83300324bf9898c85c17d41a589532026d5
NewReferee <drakone2010@gmail.com>
2019-03-07 10:27:10 +0300

    Вывод контента гостевой заменен на редирект

--- add_project.php
+++ add_project.php
@@ -2,15 +2,8 @@
 require_once ('init.php');
 
 if (!isset($_SESSION['current_user'])) {  // Если сессия пуста, показываем гостевую страницу
-	$page_content = include_template ('guest.php', [
-
-		]);
-	$layout_content = include_template ('layout-guest.php', [
-		'content' => $page_content, 
-		'title' => $page_title
-		]);
-
-	print ($layout_content);
+	header("Location: ./");
+	die ();
 } 
 
 if (!isset($_POST['name'])) {
